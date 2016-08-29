116221c36acd5d99e7373d4585d7b6926a6cb0c1
JOSE DANIEL FANDIÐO DE LA HOZ <jd.fandino10@sis.virtual.uniandes.edu.co>
2016-08-28 20:53:50 -0500

    Entities de sensor y medicion

---
+++ app/models/MedicionEntity.java
@@ -0,0 +1,77 @@
+package models;
+
+import com.avaje.ebean.Model;
+import sun.management.Sensor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "medicionentity")
+public class MedicionEntity extends Model{
+
+    public static Finder<Long, MedicionEntity> FINDER = new Finder<>(MedicionEntity.class);
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE,generator = "Medicion")
+    private Long id;
+    private double valor;
+    private Date fecha;
+    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @JoinColumn(name="sensor_id")
+    private SensorEntity sensor;
+
+
+    public MedicionEntity() {
+        this.id=null;
+        this.valor =-1;
+        fecha=null;
+        sensor=null;
+    }
+
+    public MedicionEntity(Long id, double valor) {
+        this.id = id;
+        this.valor = valor;
+        fecha = new Date(System.currentTimeMillis());
+        sensor=null;
+    }
+
+    public MedicionEntity(Long id, double valor, Date fecha, SensorEntity sensor) {
+        this.id = id;
+        this.valor = valor;
+        this.fecha = fecha;
+        this.sensor=sensor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getFecha() {return fecha;}
+
+    public void setFecha(Date fecha){ this.fecha=fecha;}
+
+    public SensorEntity getSensor(){ return sensor; }
+
+    public void setSensor(SensorEntity sensor){ this.sensor=sensor;}
+
+    @Override
+    public String toString() {
+        return "MedicionEntity{" +
+                "id=" + id +
+                ", valor='" + valor + "\'"+
+                ", fecha="+fecha.getTime()+"}";
+    }
+}
---
+++ app/models/SensorEntity.java
@@ -0,0 +1,77 @@
+package models;
+
+import com.avaje.ebean.Model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "sensorentity")
+public class SensorEntity extends Model{
+
+    public static Finder<Long, SensorEntity> FINDER = new Finder<>(SensorEntity.class);
+
+    public static final int FLUIDO = 0;
+    public static final int ENERGIA = 1;
+    public static final int TEMPERATURA = 2;
+    public static final int EMERGENCIA = 3;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE,generator = "Sensor")
+    private Long id;
+    private String nombre;
+    private int tipo_sensor;
+    @OneToMany(mappedBy = "sensor")
+    private List<MedicionEntity> mediciones;
+
+
+    public SensorEntity() {
+        this.id=null;
+        this.nombre ="NO NAME";
+    }
+
+    public SensorEntity(Long id) {
+        this();
+        this.id = id;
+    }
+
+    public SensorEntity(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTipo() {return tipo_sensor;}
+
+    public void setTipo(int tipo_sensor){ this.tipo_sensor=tipo_sensor;}
+
+    public List<MedicionEntity> getMediciones(){ return mediciones;}
+
+    public void setMediciones(List<MedicionEntity> mediciones){ this.mediciones=mediciones;}
+
+    public void addMedicion(MedicionEntity medicion){ this.mediciones.add(medicion);}
+
+    @Override
+    public String toString() {
+        return "SensorEntity{" +
+                "id=" + id +
+                ", nombre='" + nombre + "\'"+
+                ", tipo="+tipo_sensor+
+                ", mediciones="+mediciones.toString()+"}";
+    }
+}
